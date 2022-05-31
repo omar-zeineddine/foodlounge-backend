05d168f23b0e3050e961454137f40fc3236dbec0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-05-31 23:25:24 +0300

    first commit

---
+++ db/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: * ");
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "restoratedb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+?>
